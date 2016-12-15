80d00791c31182eb7a541975c150a789adbad4a7
DanSnow <dododavid006@gmail.com>
2016-12-15 20:14:58 +0800

    refactor

--- src/console.cr
+++ src/console.cr
@@ -1,27 +1,26 @@
 require "./commands/*"
 
 module EazyDB
+  COMMANDS = {
+    /^create (?<args>.*)/ => Commands::Create
+  }
   class Console
     def run
       loop do
         line = Readline.readline("> ")
         break if line.nil?
         line = line.chomp
-        case line
-        when "exit", "quit"
-          break
-        when /^create/
-          m = line.match(/^create (?<arg>.+)/).not_nil!
+        break if line == "exit" || line == "quit"
+        found = COMMANDS.each do |regex, klass|
+          m = regex.match line
           if m
-            arg = m["arg"]
-            cmd = Commands::Create.new
-            cmd.run(arg)
-          else
-            STDERR.puts "Syntax error"
+            cmd = klass.new
+            cmd.run(m["args"])
+            break :found
           end
-        else
-          puts line
         end
+
+        puts line if found != :found
       end
     end
   end
