b46b424dedea3bb74efc715cc79064702ab4a3ab
DanSnow <dododavid006@gmail.com>
2016-12-20 13:35:51 +0800

    refactor: Modulize

--- src/record.cr
+++ src/record.cr
@@ -201,17 +201,6 @@ module EazyDB::Record
       end
     end
 
-    def open_file(filepath, flag)
-      mode = if flag == "w+"
-               LibC::O_RDWR
-             else
-               LibC::O_WRONLY
-             end
-      oflag = mode | LibC::O_CLOEXEC | LibC::O_CREAT
-      fd = LibC.open(filepath, oflag, File::DEFAULT_CREATE_MODE)
-      file = IO::FileDescriptor.new(fd, blocking: true)
-    end
-
     def header
       @header.not_nil!
     end
---
+++ src/record/utils.cr
@@ -0,0 +1,14 @@
+module EazyDB::Record
+  class Record
+    def open_file(filepath, flag)
+      mode = if flag == "w+"
+               LibC::O_RDWR
+             else
+               LibC::O_WRONLY
+             end
+      oflag = mode | LibC::O_CLOEXEC | LibC::O_CREAT
+      fd = LibC.open(filepath, oflag, File::DEFAULT_CREATE_MODE)
+      file = IO::FileDescriptor.new(fd, blocking: true)
+    end
+  end
+end
